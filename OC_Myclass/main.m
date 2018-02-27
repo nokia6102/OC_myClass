25e7c694c929ff07af697d2b83430b13164ec75a
a <nokia6102@gmail.com>
2018-02-27 17:35:40 +0800

    Initial Commit

---
+++ OC_Myclass/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  OC_Myclass
+//
+//  Created by Primax on 2018/2/27.
+//  Copyright © 2018年 Primax. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
